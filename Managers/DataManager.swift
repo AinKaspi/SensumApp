c87b75f93e8aa681bb21861f34b08262dc6f8385
AinKaspi <ainkaspi@gmail.com>
2025-04-09 18:25:53 +0500

    Добавлены классы DataManager и UserProfile для управления данными пользователя. Реализованы методы для загрузки, сохранения и обновления профиля пользователя, а также расчета необходимого опыта для повышения уровня. Улучшена структура кода и добавлены комментарии для повышения читаемости.

---
+++ Managers/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  SensumApp
+//
+//  Created by Ain on 09/04/2025.
+//
+
+import Foundation
---
+++ Models/UserProfile.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfile.swift
+//  SensumApp
+//
+//  Created by Ain on 09/04/2025.
+//
+
+import Foundation
