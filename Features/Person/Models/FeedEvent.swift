6f14e6a227f4931a8d74458277a62f315edd3a02
AinKaspi <ainkaspi@gmail.com>
2025-04-21 23:43:37 +0500

    Remove obsolete files and refactor user profile management: Deleted EventsViewController, UserProfileCoordinator, UserProfileContainerViewController, FeedEvent model, FullFeedViewController, SettingsViewController, UserProfileStatsViewController, and TopMenuView. Introduced UserProfileCardViewController to streamline user profile display and management. Enhanced project structure for improved navigation and user experience.

--- Features/Person/Models/FeedEvent.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct FeedEvent {
-    let id: String
-    let description: String
-    let timestamp: Date
-    // TODO: Добавить другие свойства (тип события, связанный пользователь/достижение?)
-}
--- Features/Person/Scenes/FullFeed/FullFeedViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class FullFeedViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemGreen // Зеленый фон для заглушки
-        title = "Вся Лента" // Устанавливаем заголовок
-    }
-}
